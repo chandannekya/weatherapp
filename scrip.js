81bc88e79a0f4236569ca35d8d5d9821e5896f2e
Chandan <chadan85338818@gmail.com>
2023-12-22 16:34:54 +0530

    fisnish

---
+++ scrip.js
@@ -0,0 +1,43 @@
+const inpb=document.querySelector("input");
+const  searchbt=document.querySelector("button");
+const weatherimg=document.querySelector("#imgw");
+const temp=document.querySelector("#temp");
+const humidity =document.querySelector("#humidity");
+const windspeed=document.querySelector("#wind");
+const dis=document.querySelector("#city");
+
+async function weatherupdate(city){
+ 
+    const api_key="20ea4dbb2899183311392147b8289543";
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+  const weather_data=await fetch(`${url}`).then(response =>response.json());
+  temp.innerHTML=`${Math.round(weather_data.main.temp-273.15)+"°C"}`;
+  
+  
+  
+  humidity.innerHTML=`${weather_data.main.humidity +"%"}`;
+  windspeed.innerHTML=`${weather_data.wind.speed+"KM/h"}`;
+  dis.innerHTML=`${weather_data.name}`;
+  if(weather_data.weather[0].description=="fog"){
+    weatherimg.src="fog.png";
+  }
+  else if(weather_data.weather[0].description=="cloudy"){
+    weatherimg.src="cloudy.png";
+  }
+  else if(weather_data.weather[0].description=="haze"){
+    weatherimg.src="haze.png";
+  }
+  else if(weather_data.weather[0].description=="clear"){
+    weatherimg.src="sun.png";
+  }
+  else if(weather_data.weather[0].description=="rain"){
+    weatherimg.src="rain(1).png";
+  }
+}
+
+
+
+searchbt.addEventListener("click",()=>{
+    weatherupdate(inpb.value);
+    
+});
